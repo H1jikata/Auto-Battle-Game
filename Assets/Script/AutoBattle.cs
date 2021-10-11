39eef69c16d89f59a79d725a074a6a1a1ffd0fba
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-10-11 20:16:45 +0900

    BattleScine1~5

---
+++ Assets/Script/AutoBattle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoBattle : MonoBehaviour
+{
+    [SerializeField] GameObject m_target = null;
+    float m_time = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_time += Time.deltaTime;
+    }
+}

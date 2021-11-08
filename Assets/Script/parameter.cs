1d1d4db2d3da3a1e14726649b56b6c737e0e1d83
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-11-08 16:31:38 +0900

    parameter

---
+++ Assets/Script/parameter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parameter : ScriptableObject
+{
+    [SerializeField] int _hp;
+    [SerializeField] int _power;
+    [SerializeField] int _def;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int HP
+    {
+        get { return _hp; }
+    }
+
+    public int Power
+    {
+        get { return _power; }
+    }
+
+    public int Defense
+    {
+        get { return _def; }
+    }
+}

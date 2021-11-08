21bd8363a0aa890125c9f3d7366e8b5f1591c079
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-11-08 18:19:13 +0900

    test

--- Assets/Script/parameter.cs
+++ Assets/Script/parameter.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "GameParameter", menuName = "GameParameter")]
 public class parameter : ScriptableObject
 {
     [SerializeField] int _hp;
     [SerializeField] int _power;
     [SerializeField] int _def;
+    [SerializeField] float _ultTime;
     void Start()
     {
         
@@ -31,5 +33,10 @@ public class parameter : ScriptableObject
     public int Defense
     {
         get { return _def; }
+    }
+
+    public float Ult
+    {
+        get { return _ultTime; }
     }
 }

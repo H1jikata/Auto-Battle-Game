be097f2144f6f37680c39a1601fa6a2fd9551809
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-11-29 16:14:23 +0900

    途中経過

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>ボタンの効果音</summary>
+    [SerializeField] AudioSource _audio;
+
+    //GameStart_"Select"にシーン遷移
+    public void GameStart()
+    {
+        _audio.Play();
+        SceneManager.LoadScene("Select");
+    }
+
+    public void Stage1()
+    {
+        _audio.Play();
+        SceneManager.LoadScene("Battle1");
+    }
+}

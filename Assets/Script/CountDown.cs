3d3babce5b84f30a15f1e4cc1da6d8e9cff95e0c
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-12-13 15:42:28 +0900

    とりあえず

---
+++ Assets/Script/CountDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] float _Time = 3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Time = Time.deltaTime;
+    }
+}

8e1dc7d129121ce579933532f0a58e6e99026feb
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-02-16 23:36:32 +0900

    Damageポップアップ

--- Assets/Script/EnemyStatus.cs
+++ Assets/Script/EnemyStatus.cs
@@ -8,11 +8,17 @@ public class EnemyStatus : MonoBehaviour,IDamage
     [SerializeField] float _defence = 3;
     [SerializeField] float _currentHp = 20;
 
+    [SerializeField] GameObject _damageUI = default;
+    [SerializeField] GameObject _posObj = default;
+    [SerializeField] Vector3 _adjPos = default;
 
     public void GetDamage(float damege)
     {
         _enemyMaxHp -= Mathf.Abs(Defence - damege);
-        Debug.Log(_enemyMaxHp);
+
+        Instantiate(_damageUI);
+        _damageUI.GetComponent<TextMesh>().text = (_currentHp - _enemyMaxHp).ToString();
+        _damageUI.transform.position = _posObj.transform.position + _adjPos;
         if (_enemyMaxHp <= 0)
         {
             Debug.Log("Enemyを倒した");
